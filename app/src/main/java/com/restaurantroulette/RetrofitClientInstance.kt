707bfb266c74f930f8cddfeab6f5eb30eeef4282
rosasmn <rosasmn@mail.uc.edu>
2020-03-05 13:11:23 -0500

    Creating RetrofitClientInstance file and adding the libraries to the app build.gradle

---
+++ app/src/main/java/com/restaurantroulette/RetrofitClientInstance.kt
@@ -0,0 +1,22 @@
+package com.restaurantroulette
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClientInstance {
+    private var retrofit: Retrofit? = null
+    private val BASE_URL = "https://opentable.herokuapp.com/"
+
+    val retrofitInstance : Retrofit?
+        get() {
+            // checks if this object has been created
+            if (retrofit == null) {
+                // if not, it creates it here
+                retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+}
