cd465ea9f4ef0d7410def927b594f890a01e6af7
Rodgerskc <Rodgerskc@gmail.com>
2020-03-07 16:57:44 -0500

    Random Button

--- app/src/main/java/com/restaurantroulette/MainActivity.kt
+++ app/src/main/java/com/restaurantroulette/MainActivity.kt
@@ -2,6 +2,9 @@ package com.restaurantroulette
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.widget.Button
+import android.widget.TextView
 import com.restaurantroulette.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,20 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        val btnRandom = findViewById<Button>(R.id.btnRadom)
+        val msgMain = findViewById<TextView>(R.id.message)
+        btnRandom?.setOnClickListener{
+            btnRandomClick(msgMain)
+        }
+
     }
 
+    /**
+     * Logic for Selecting a random Restaraunt Location.
+     * @param msg TextView of Main Fragment.
+     */
+    fun btnRandomClick(msg: TextView){
+        msg.text = "Random selection enabled, hit search to continue."
+        println("${msg.text}")
+    }
 }
