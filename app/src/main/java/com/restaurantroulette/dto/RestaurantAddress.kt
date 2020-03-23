b2ba5ed89ecfe6dfa887c44b55c781c2088d4935
rosasmn <rosasmn@mail.uc.edu>
2020-03-23 10:20:43 -0400

    Removing SerializedName import from RestaurantAddress.kt file

--- app/src/main/java/com/restaurantroulette/dto/RestaurantAddress.kt
+++ app/src/main/java/com/restaurantroulette/dto/RestaurantAddress.kt
@@ -1,7 +1,5 @@
 package com.restaurantroulette.dto
 
-import com.google.gson.annotations.SerializedName
-
 data class RestaurantAddress(var address : String) {
     override fun toString(): String {
         return address
