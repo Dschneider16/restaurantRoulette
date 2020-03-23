4ceab2db8ebbe431fdd273a1911eb09ac2b1e3f3
rosasmn <rosasmn@mail.uc.edu>
2020-03-23 10:49:34 -0400

    Adding a couple more attributes to RestaurantAddress DTO and creating RestaurantDetails DTO

--- app/src/main/java/com/restaurantroulette/dto/RestaurantAddress.kt
+++ app/src/main/java/com/restaurantroulette/dto/RestaurantAddress.kt
@@ -1,6 +1,8 @@
 package com.restaurantroulette.dto
 
-data class RestaurantAddress(var address : String) {
+import com.google.gson.annotations.SerializedName
+
+data class RestaurantAddress(var address : String, var city : String, var state : String, @SerializedName("postal_code") var postalCode : Int) {
     override fun toString(): String {
         return address
     }
