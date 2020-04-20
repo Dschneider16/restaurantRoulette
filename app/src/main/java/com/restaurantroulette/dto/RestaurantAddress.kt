cbf662b4d893a27d2de489077764a2d76ea2cb77
rosasmn <rosasmn@mail.uc.edu>
2020-04-20 11:20:54 -0400

    Taking commas out of some of the DTO return statements

--- app/src/main/java/com/restaurantroulette/dto/RestaurantAddress.kt
+++ app/src/main/java/com/restaurantroulette/dto/RestaurantAddress.kt
@@ -5,6 +5,6 @@ import com.restaurantroulette.dto.City
 
 data class RestaurantAddress(var address : String, var city : String, var state : String, @SerializedName("postal_code") var postalCode : Int) {
     override fun toString(): String {
-        return "$address, $city, $state, $postalCode"
+        return "$address $city, $state $postalCode"
     }
 }
